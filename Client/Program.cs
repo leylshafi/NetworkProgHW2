ecdf5609d9e76f311d006c786f3c283d3fc7ea32
Leyla Shafiyeva <llshafi03@gmail.com>
2023-03-17 21:54:34 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,126 @@
+﻿using Client;
+using System.Net.Sockets;
+using System.Text.Json;
+
+var client = new TcpClient("127.0.0.1", 45678);
+
+var stream = client.GetStream();
+
+var bw = new BinaryWriter(stream);
+var br = new BinaryReader(stream);
+
+var userCommand = string.Empty;
+
+while(true)
+{
+    Console.WriteLine("Enter commandText, then Param: ");
+    userCommand = Console.ReadLine();
+    if(userCommand is not null)
+    {
+        var temp = userCommand.Split(' ');
+        var command = new Command()
+        {
+            Text = temp[0],
+            Param = temp[1]
+        };
+
+        switch (command.Text.ToLower())
+        {
+            case Command.HELP:
+                if (!string.IsNullOrWhiteSpace(command.Param))
+                {
+                    Console.WriteLine(" Wrong Syntax ");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                var jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+                var response = br.ReadString();
+                Console.WriteLine(response);
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case Command.PROCLIST:
+                if (!string.IsNullOrWhiteSpace(command.Param))
+                {
+                    Console.WriteLine(" Wrong Syntax ");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+
+                jsonStr = JsonSerializer.Serialize(command);
+                bw.Write(jsonStr);
+                await Task.Delay(50);
+                response = br.ReadString();
+                Console.WriteLine(response);
+                Console.ReadKey();
+                Console.Clear();
+                break;
+            case Command.KILL:
+                //CalculatorApp
+                if (string.IsNullOrWhiteSpace(command.Param))
+                {
+                    Console.WriteLine(" Wrong Syntax ");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                jsonStr = JsonSerializer.Serialize(command);
+
+                bw.Write(jsonStr);
+
+                await Task.Delay(50);
+
+                var responseBool = br.ReadBoolean();
+                if (responseBool is true)
+                {
+                    Console.WriteLine("Process succesfully ended");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Process couldn't ended");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                break;
+            case Command.RUN:
+                if (string.IsNullOrWhiteSpace(command.Param))
+                {
+                    Console.WriteLine(" Wrong Syntax ");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                jsonStr = JsonSerializer.Serialize(command);
+
+                bw.Write(jsonStr);
+
+                await Task.Delay(50);
+
+                responseBool = br.ReadBoolean();
+                if (responseBool is true)
+                {
+                    Console.WriteLine("Process succesfully run");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Process couldn't run");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,124 @@
+﻿using Server;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+var listener = new TcpListener(IPAddress.Parse("127.0.0.1"),45678);
+listener.Start();
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    new Task(() =>
+    {
+        var stream = client.GetStream();
+        var bw = new BinaryWriter(stream);
+        var br = new BinaryReader(stream);
+        while (true)
+        {
+            var jsonStr = br.ReadString();
+
+            var command = JsonSerializer.Deserialize<Command>(jsonStr);
+
+            if (command is null)
+                return;
+
+            switch (command.Text.ToLower())
+            {
+                case Command.HELP:
+                    {
+                        var helpText = HelpText();
+                        bw.Write(helpText);
+                        stream.Flush();
+                        break;
+                    }
+                case Command.PROCLIST:
+                    {
+                        var jsonList = GetProcesses();
+                        bw.Write(jsonList);
+                        stream.Flush();
+                        break;
+                    }
+                case Command.KILL:
+                    {
+                        var canKill = KillProcess(command.Param);
+                        bw.Write(canKill);
+                        break;
+                    }
+                case Command.RUN:
+                    {
+                        var canRun = RunProcess(command.Param);
+                        bw.Write(canRun);
+                        break;
+                    }
+                default:
+                    break;
+            }
+
+        }
+
+
+
+
+    }).Start();
+
+    string HelpText()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append("\nproclist ------ see all processes");
+        builder.Append("\nkill <process name> ------ end process");
+        builder.Append("\nrun <process name> ------ run process");
+
+        return builder.ToString();
+    }
+    bool KillProcess(string? processName)
+    {
+        if (processName is not null)
+        {
+            
+            var processes = Process.GetProcessesByName(processName);
+
+            if (processes.Length > 0)
+            {
+                try
+                {
+                    foreach (var p in processes)
+                        p.Kill();
+
+                    return true;
+                }
+                catch (Exception) { }
+            }
+            return false;
+
+        }
+        else return false;
+    }
+
+    bool RunProcess(string? processName)
+    {
+        if (processName is not null)
+        {
+            //CalculatorApp
+
+            try
+            {
+                Process.Start(processName);
+                return true;
+            }
+            catch (Exception) { return false; }
+
+        }
+        else return false;
+    }
+
+    string GetProcesses()
+    {
+        var list = Process.GetProcesses();
+        var names=list.Select(p => p.ProcessName);
+        var jsonList = JsonSerializer.Serialize(names);
+
+        return jsonList;
+    }
+}
